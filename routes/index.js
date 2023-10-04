d0dbe749d06a41a3127c1d43dc003364361dc7bf
Andreas Rayias <wowcaeu.andreas@gmail.com>
2023-10-04 23:56:03 +0100

    added routing for /new to submit a form that will eventually post and redirect to root

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Message Board", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "Message Board" });
+});
+
+router.post("/new", function (req, res, next) {
+  res.render("form", { title: "Message Board" });
 });
 
 module.exports = router;
